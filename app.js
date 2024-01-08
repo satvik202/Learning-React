3ca3bfc0ff3fd2b037f8e80ce107158087130053
satvik <7vikpandey@gmail.com>
2024-01-08 20:07:42 +0530

    learned about parcels

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client" ;
 const heading= React.createElement("div", {id : "child1"},
 [React.createElement("h1",{}, "this is an h1 tag"),
 React.createElement("h2",{}, "this is an h2 tag")]
