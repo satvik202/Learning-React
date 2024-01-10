f90f42d855ca36314f19daedfcfb245d0d80b10e
satvik <7vikpandey@gmail.com>
2024-01-10 19:27:28 +0530

    learned about react components and jsx

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client" ;
-const heading= React.createElement("div", {id : "child1"},
-[React.createElement("h1",{}, "this is an h1 tag"),
-React.createElement("h2",{}, "this is an h2 tag")]
-);
-const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+import ReactDOM from "react-dom/client";
+
+const jsxElement = <h1>I am Satvik! And I am learning React</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const Heading = () => <h1>What is this?</h1>;
+
+const Component = () => {
+  return (
+    <div>
+      {jsxElement}
+      <Heading />
+
+      <h1>This is a react Component</h1>
+    </div>
+  );
+};
+
+// root.render(jsxElement);
+root.render(<Component />);
